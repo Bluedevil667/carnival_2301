6da40fc6b0855fa06854379c11e532e4f75728af
Logan Wilson <loganwilson777@gmail.com>
2023-02-27 09:29:15 -0700

    adds visitor spec, and atributes to the visitor class

---
+++ spec/visitor_spec.rb
@@ -0,0 +1,16 @@
+require 'spec_helper'
+RSpec.describe Visitor do
+  
+  it 'exists' do
+    visitor1 = Visitor.new("Bruce", 54, 10)
+    expect(visitor1).to be_a Visitor
+  end
+
+  it 'has atributes' do
+    visitor1 = Visitor.new("Bruce", 54, 10)
+    expect(visitor1.name).to eq("Bruce")
+    expect(visitor1.height).to eq(54)
+    expect(visitor1.spending_money).to eq(10)
+    expect(visitor1.preferences).to eq([])
+  end
+end
